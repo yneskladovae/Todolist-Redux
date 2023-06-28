90a0d79aa6c8087e2e5d174f180b0933c02ac442
yneskladovae <yneskladovae27@gmail.com>
2023-06-28 11:38:13 +0300

    refactor: change tab size

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -3,32 +3,32 @@ import {authAPI} from '../api/todolists-api'
 import {setIsLoggedInAC} from '../features/Login/auth-reducer'
 
 const initialState: InitialStateType = {
-    status: 'idle',
-    error: null,
-    isInitialized: false
+  status: 'idle',
+  error: null,
+  isInitialized: false
 }
 
 export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
-    switch (action.type) {
-        case 'APP/SET-STATUS':
-            return {...state, status: action.status}
-        case 'APP/SET-ERROR':
-            return {...state, error: action.error}
-        case 'APP/SET-IS-INITIALIED':
-            return {...state, isInitialized: action.value}
-        default:
-            return {...state}
-    }
+  switch (action.type) {
+    case 'APP/SET-STATUS':
+      return {...state, status: action.status}
+    case 'APP/SET-ERROR':
+      return {...state, error: action.error}
+    case 'APP/SET-IS-INITIALIED':
+      return {...state, isInitialized: action.value}
+    default:
+      return {...state}
+  }
 }
 
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 export type InitialStateType = {
-    // происходит ли сейчас взаимодействие с сервером
-    status: RequestStatusType
-    // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
-    error: string | null
-    // true когда приложение проинициализировалось (проверили юзера, настройки получили и т.д.)
-    isInitialized: boolean
+  // происходит ли сейчас взаимодействие с сервером
+  status: RequestStatusType
+  // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
+  error: string | null
+  // true когда приложение проинициализировалось (проверили юзера, настройки получили и т.д.)
+  isInitialized: boolean
 }
 
 export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
@@ -36,20 +36,20 @@ export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-ST
 export const setAppInitializedAC = (value: boolean) => ({type: 'APP/SET-IS-INITIALIED', value} as const)
 
 export const initializeAppTC = () => (dispatch: Dispatch) => {
-    authAPI.me().then(res => {
-        if (res.data.resultCode === 0) {
-            dispatch(setIsLoggedInAC(true));
-        } else {
+  authAPI.me().then(res => {
+    if (res.data.resultCode === 0) {
+      dispatch(setIsLoggedInAC(true));
+    } else {
 
-        }
-        dispatch(setAppInitializedAC(true));
-    })
+    }
+    dispatch(setAppInitializedAC(true));
+  })
 }
 
 export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
 export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
 
 type ActionsType =
-    | SetAppErrorActionType
-    | SetAppStatusActionType
-    | ReturnType<typeof setAppInitializedAC>
+  | SetAppErrorActionType
+  | SetAppStatusActionType
+  | ReturnType<typeof setAppInitializedAC>
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,17 @@
 import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
 import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
-import { AnyAction, applyMiddleware, combineReducers, createStore } from 'redux'
-import thunkMiddleware, { ThunkAction, ThunkDispatch } from 'redux-thunk'
+import {AnyAction, applyMiddleware, combineReducers, createStore} from 'redux'
+import thunkMiddleware, {ThunkAction, ThunkDispatch} from 'redux-thunk'
 import {appReducer} from './app-reducer'
 import {authReducer} from '../features/Login/auth-reducer'
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
-    tasks: tasksReducer,
-    todolists: todolistsReducer,
-    app: appReducer,
-    auth: authReducer
+  tasks: tasksReducer,
+  todolists: todolistsReducer,
+  app: appReducer,
+  auth: authReducer
 })
 // непосредственно создаём store
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
