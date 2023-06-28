47575b1175109c0456d573df9ad7b8440ad81e8e
yneskladovae <yneskladovae27@gmail.com>
2023-06-28 11:34:08 +0300

    refactor: change let with const, use useAppSelector

--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -2,36 +2,35 @@ import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {AppRootStateType} from '../../app/store'
 import {setAppErrorAC} from '../../app/app-reducer'
-import { AlertProps, Snackbar } from '@mui/material'
+import {AlertProps, Snackbar} from '@mui/material'
 import MuiAlert from '@mui/material/Alert';
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
-	props,
-	ref,
+  props,
+  ref,
 ) {
-	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 export function ErrorSnackbar() {
 
-    const error = useSelector<AppRootStateType, string | null>(state => state.app.error);
-    const dispatch = useDispatch()
+  const error = useSelector<AppRootStateType, string | null>(state => state.app.error);
+  const dispatch = useDispatch()
 
-    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
-        if (reason === 'clickaway') {
-            return
-        }
-        dispatch(setAppErrorAC(null));
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return
     }
+    dispatch(setAppErrorAC(null));
+  }
 
+  const isOpen = error !== null;
 
-    const isOpen = error !== null;
-
-    return (
-        <Snackbar open={isOpen} autoHideDuration={6000} onClose={handleClose}>
-					<Alert onClose={handleClose} severity="error">
-						{error}
-					</Alert>
-				</Snackbar>
-    )
+  return (
+    <Snackbar open={isOpen} autoHideDuration={6000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity="error">
+        {error}
+      </Alert>
+    </Snackbar>
+  )
 }
--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -4,61 +4,61 @@ import {authAPI, LoginParamsType} from '../../api/todolists-api'
 import {handleServerAppError, handleServerNetworkError} from '../../utils/error-utils'
 
 const initialState: InitialStateType = {
-    isLoggedIn: false
+  isLoggedIn: false
 }
 
 export const authReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
-    switch (action.type) {
-        case 'login/SET-IS-LOGGED-IN':
-            return {...state, isLoggedIn: action.value}
-        default:
-            return state
-    }
+  switch (action.type) {
+    case 'login/SET-IS-LOGGED-IN':
+      return {...state, isLoggedIn: action.value}
+    default:
+      return state
+  }
 }
 
 // actions
 
 export const setIsLoggedInAC = (value: boolean) =>
-    ({type: 'login/SET-IS-LOGGED-IN', value} as const)
+  ({type: 'login/SET-IS-LOGGED-IN', value} as const)
 
 
 // thunks
 export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch<ActionsType | SetAppStatusActionType | SetAppErrorActionType>) => {
-    dispatch(setAppStatusAC('loading'))
-    authAPI.login(data)
-        .then(res => {
-            if (res.data.resultCode === 0) {
-                dispatch(setIsLoggedInAC(true))
-                dispatch(setAppStatusAC('succeeded'))
-            } else {
-                handleServerAppError(res.data, dispatch)
-            }
-        })
-        .catch((error) => {
-            handleServerNetworkError(error, dispatch)
-        })
+  dispatch(setAppStatusAC('loading'))
+  authAPI.login(data)
+    .then(res => {
+      if (res.data.resultCode === 0) {
+        dispatch(setIsLoggedInAC(true))
+        dispatch(setAppStatusAC('succeeded'))
+      } else {
+        handleServerAppError(res.data, dispatch)
+      }
+    })
+    .catch((error) => {
+      handleServerNetworkError(error, dispatch)
+    })
 }
 export const logoutTC = () => (dispatch: Dispatch<ActionsType | SetAppStatusActionType | SetAppErrorActionType>) => {
-    dispatch(setAppStatusAC('loading'))
-    authAPI.logout()
-        .then(res => {
-            if (res.data.resultCode === 0) {
-                dispatch(setIsLoggedInAC(false))
-                dispatch(setAppStatusAC('succeeded'))
-            } else {
-                handleServerAppError(res.data, dispatch)
-            }
-        })
-        .catch((error) => {
-            handleServerNetworkError(error, dispatch)
-        })
+  dispatch(setAppStatusAC('loading'))
+  authAPI.logout()
+    .then(res => {
+      if (res.data.resultCode === 0) {
+        dispatch(setIsLoggedInAC(false))
+        dispatch(setAppStatusAC('succeeded'))
+      } else {
+        handleServerAppError(res.data, dispatch)
+      }
+    })
+    .catch((error) => {
+      handleServerNetworkError(error, dispatch)
+    })
 }
 
 // types
 
 type ActionsType = ReturnType<typeof setIsLoggedInAC>
 type InitialStateType = {
-    isLoggedIn: boolean
+  isLoggedIn: boolean
 }
 
 type ThunkDispatch = Dispatch<ActionsType | SetAppStatusActionType | SetAppErrorActionType>
